7be7b076bbec69ce4dbcb48b1974372cccdf5648
TaylorDelta <taylor.delta@protonmail.com>
2025-11-28 23:32:23 +0100

    Add files via upload
    Framework based on https://rtw.b-robotized.com/master/index.html

---
+++ src/robot_hardware_interface/include/robot_hardware_interface/robot_hardware_interface.hpp
@@ -0,0 +1,63 @@
+// Copyright (c) 2025, TaylorDelta
+// Copyright (c) 2025, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ROBOT_HARDWARE_INTERFACE__ROBOT_HARDWARE_INTERFACE_HPP_
+#define ROBOT_HARDWARE_INTERFACE__ROBOT_HARDWARE_INTERFACE_HPP_
+
+#include <string>
+#include <vector>
+
+#include "hardware_interface/actuator_interface.hpp"
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/macros.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+
+namespace robot_hardware_interface
+{
+class RobotHardwareInterface : public hardware_interface::ActuatorInterface
+{
+public:
+  hardware_interface::CallbackReturn on_init(
+    const hardware_interface::HardwareInfo & info) override;
+
+  hardware_interface::CallbackReturn on_configure(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  hardware_interface::CallbackReturn on_activate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  hardware_interface::CallbackReturn on_deactivate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  hardware_interface::return_type read(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+  hardware_interface::return_type write(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+private:
+  std::vector<double> hw_commands_;
+  std::vector<double> hw_states_;
+};
+
+}  // namespace robot_hardware_interface
+
+#endif  // ROBOT_HARDWARE_INTERFACE__ROBOT_HARDWARE_INTERFACE_HPP_
